4f8a41acf1771992cea03f06026f7fda597708d2
Success Malla <success_malla@hotmail.com>
2021-06-06 16:01:51 +0545

    Added Watermarks and tumble window

--- src/main/java/malla/StreamingJobDataStreamAPIExample.java
+++ src/main/java/malla/StreamingJobDataStreamAPIExample.java
@@ -1,28 +1,36 @@
 package malla;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class StreamingJobDataStreamAPIExample {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<String> data = env.readTextFile("file:///home/smalla/Downloads/sample_dataset.csv");
-        data.map(new MapFunction<String, Tuple2<String,Integer>>() {
+        data.map(new MapFunction<String, Tuple3<String,Integer,Timestamp>>() {
                     @Override
-                    public Tuple2<String, Integer> map(String line) throws Exception {
+                    public Tuple3<String, Integer, Timestamp> map(String line) throws Exception {
                         String[] cells = line.split(",");
-                        return new Tuple2<String, Integer>(cells[2], 1);
+                        return new Tuple3<String, Integer, Timestamp>(cells[2], 1, (new Timestamp((new Date()).getTime()) ) );
                     }
-                })
+                }).assignTimestampsAndWatermarks(
+                    WatermarkStrategy.forMonotonousTimestamps() )
                 .keyBy(v->v.f0)
-                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .reduce(new ReduceFunction<Tuple3<String, Integer, Timestamp>>() {
                     @Override
-                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t2, Tuple2<String, Integer> t1) throws Exception {
-                        return new Tuple2<String,Integer>(t1.f0,t1.f1 + t2.f1);
+                    public Tuple3<String, Integer, Timestamp> reduce(Tuple3<String, Integer, Timestamp> t2, Tuple3<String, Integer, Timestamp> t1) throws Exception {
+                        return new Tuple3<String, Integer, Timestamp>(t1.f0,t1.f1 + t2.f1, t1.f2);
                     }
                 })
                 .print();
